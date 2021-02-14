29dae871cd6d1dd8d55041cdc4d86155507fd0d8
pederzini98 <lucas.pps1998@gmail.com>
2021-02-14 20:19:24 -0300

    Change navbar text to an icon

--- src/components/NavBarComponent/index.js
+++ src/components/NavBarComponent/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap'
 
 import { useDispatch } from 'react-redux'
+import { FaShoppingCart } from "react-icons/fa";
 
 
 import { setMODAL } from '../../redux-actions/redux-actions'
@@ -25,7 +26,9 @@ function NavBarComponent() {
             <Navbar className="navbar navbar-fixed-top" >
                 <Nav className="nav-content">
                     <a className='item' href=" https://github.com/pederzini98/React--Catalago-de-Jogos" >Github</a>
-                    <Nav.Item className="item" onClick={handleOpen}>Carrinho de Compras</Nav.Item>
+                    <Nav.Item className="icon" onClick={handleOpen}>
+                        <FaShoppingCart/>
+                    </Nav.Item>
 
                 </Nav>
 
